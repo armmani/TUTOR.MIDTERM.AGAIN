15b908ef1bd569d8b26d7bd728a48418d47f18b1
armmani <arm.maniii@gmail.com>
2025-06-09 18:19:59 +0700

    app approuter setup

---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router'
+import MainLayout from '../layout/MainLayout'
+import LoginPage from '../pages/LoginPage'
+import TodoListPage from '../pages/TodoListPage'
+import RegisterPage from '../pages/RegisterPage'
+
+function AppRouter() {
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<MainLayout /> }>
+            <Route index element={<LoginPage />} />
+            <Route path='todo' element={<TodoListPage />} />
+            <Route path='register' element={<RegisterPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  )
+}
+
+export default AppRouter
